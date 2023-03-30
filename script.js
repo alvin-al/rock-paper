d6efe6070d05a358dedb628a81dfb3fdecaf94af
Alvin Al <50256064+alvvxal@users.noreply.github.com>
2023-03-31 06:26:06 +0700

    Changes on game function, but need to add the result of game.

---
+++ script.js
@@ -0,0 +1,42 @@
+let pick = ['Rock','Paper','Scissors'];
+
+// Comp choice
+function getComputerChoice() {
+    let randomComp = pick[Math.floor(Math.random()*pick.length)]; 
+    return randomComp;
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection + computerSelection) {
+    case 'rockRock':
+    case 'paperPaper':
+    case 'scissorsScissors':
+      return 'Tie';
+    case 'rockScissors':
+    case 'paperRock':
+    case 'scissorsPaper':
+      return 'Win';
+    case 'rockPaper':
+    case 'paperScissors':
+    case 'scissorsRock':
+      return 'Lose!';
+    default:
+      return 'Repeat your choice';
+  }
+}
+  
+
+
+let result = [];
+function game(){
+  for (let i = 0; i < 5; i++){
+    const playerSelection = prompt('What do you choose? ');
+    const computerSelection = getComputerChoice();
+    console.log(`You : ${playerSelection} vs Comp : ${computerSelection}.`);
+    result.push(playRound(playerSelection, computerSelection));
+  }
+}
+result[];
+game();
